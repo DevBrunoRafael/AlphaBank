4f3cfd4000669c17fa3db935bdd2bb98b350c403
Bruno Rafael <88515518+DevBrunoRafael@users.noreply.github.com>
2022-07-01 01:32:44 -0300

    Delete EmailAcess.java

--- src/main/java/App/Components/SendingEmails/EmailAcess.java
+++
@@ -1,8 +0,0 @@
-package App.Components.SendingEmails;
-
-public abstract class EmailAcess {
-
-    public static final String USER = "devbrunorafaell@gmail.com";
-    public static final String PASSWORD = "lidnbmwbihulkabi";
-
-}
